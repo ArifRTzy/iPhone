f2d5a65c4e76c35f1f4651f2a0bb0d428bd41b61
ArifR_ <arifr0751@gmail.com>
2024-09-19 21:50:32 +0700

    initial

---
+++ src/components/VideoCarousel.jsx
@@ -0,0 +1,25 @@
+import { hightlightsSlides } from '../constants'
+
+function VideoCarousel() {
+  return (
+    <>
+    <div className="flex items-center">
+        {
+        hightlightsSlides.map((list, i)=>(
+            <div key={list.id} id='slider' className="sm:pr-20 pr-10">
+                <div className="video-crousel_container">
+                    <div className='w-full h-full flex-center rounded-3xl overflow-hidden bg-black'>
+                        <video>
+                            <source src={list.video} type='video/mp4'/>
+                        </video>
+                    </div>
+                </div>
+            </div>
+        ))
+        }
+    </div>
+    </>
+  )
+}
+
+export default VideoCarousel
