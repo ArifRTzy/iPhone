55eba6311de9a4d45576fd5af3762c74991eb7f3
ArifRTzy <arifr0751@gmail.com>
2024-09-26 09:16:00 +0700

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import HighLights from "./components/HighLIghts";
+import HighLights from "./components/HighLights";
 
 const App = () => {
   return (
